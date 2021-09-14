f2bb3a08696f1fbfb71d8fbdba1db24b1a5eb8fa
CagyJiao <kaijie.jiao@instructure.com>
2021-09-14 11:15:51 +0200

    component

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 
+const Welcome = (props) => (
+  <div>
+    <h1>Hello, {props.name}</h1>
+  </div>
+)
+
 const App = () => (
   <div>
     <h1>HELLO WORLD! HELLO REACT!</h1>
+    <Welcome name='Test'/>
   </div>
 );
 
