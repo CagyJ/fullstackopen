63076d9f5ea82b11317f0e1bf7ca5c45abcca53e
CagyJiao <kaijie.jiao@instructure.com>
2021-09-15 13:50:48 +0200

    state hook example

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Welcome = (props) => (
   <div>
     <h1>Hello, {props.name}</h1>
   </div>
 )
+
+const HookExample = () => {
+  // '0' is the initial state for the variable('count')
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times.</p>
+      <button onClick={ () => setCount(count + 1)}>
+        Click me!
+      </button>
+      <button onClick={ () => setCount(0)}>
+        ZERO
+      </button>
+    </div>
+  )
+}
 
 // const App = () => {
 //   const now = new Date();
@@ -31,6 +48,9 @@ const App = () => {
     ),
     React.createElement(
       'h1', null, 'Now: ', now.toString()
+    ),
+    React.createElement(
+      HookExample, null
     )
   );
 };
