e7e710d580f1c793e7531dc6afce4b499f27417a
CagyJiao <kaijie.jiao@instructure.com>
2021-09-15 12:36:31 +0200

    ex1.5

--- part1/exercise1/src/App.js
+++ part1/exercise1/src/App.js
@@ -28,30 +28,32 @@ const Total = (props) => {
 
 const App = () => {
   
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name : 'Fundamentals of React',
-      exercises : 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises : 7
-    },
-    {
-      name : 'State of a component',
-      exercises : 14
-    }
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
   
 
   // '...' will seperately assign the variables from props into props, else, the props will be encapulated into props
   // more info: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
   return (
     <div>
-      <Header course={course} />
-      <Content {...parts} />
-      <Total {...parts} />
+      <Header course={course.name} />
+      <Content {...course.parts} />
+      <Total {...course.parts} />
     </div>
   )
 }
