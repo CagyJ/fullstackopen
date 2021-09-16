99e93015e8442091be967fcc596a38b2b49d11fd
CagyJiao <kaijie.jiao@instructure.com>
2021-09-16 18:37:06 +0200

    add sum and map more courses

--- part2/exercise2/src/App.js
+++ part2/exercise2/src/App.js
@@ -22,39 +22,76 @@ const Content = ({parts}) => {
   );
 };
 
+const Summary = ({parts}) => {
+  return (
+    <h4>total of {parts.reduce((prev, cur)=>(prev + cur.exercises),0)} exercises</h4>
+  )
+}
+
 const Course = ({course}) => {
   return (
     <div>
       <Header text={course.name} />
       <Content parts={course.parts} />
+      <Summary parts={course.parts} />
     </div>
   )
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    <div>
+      {courses.map((course, i) =>
+        <Course key={i} course={course} />
+      )}
+      
+    </div>
+  )
 }
 
 export default App;
