45aac767258e223a24db0b6c473c896b08b2d767
CagyJiao <kaijie.jiao@instructure.com>
2021-09-16 17:30:51 +0200

    refactoring and import

--- part2/part2/src/App.js
+++ part2/part2/src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
+import Note from './components/Note'
 
-const App = (props) => {
-  const { notes } = props;
-
+const App = ( {notes} ) => {
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {notes.map(note => 
-          <li key={note.id}>
-            {note.content}
-          </li>
+          <Note key={note.id} note={note} />
         )}
       </ul>
-      <ul>
+      {/* <ul>
         {notes.map((note, i) =>
           <li key={i}>
             {note.content}
           </li>
         )}
-      </ul>
+      </ul> */}
     </div>
   )
 }
