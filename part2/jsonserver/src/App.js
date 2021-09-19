159d82dd9aafd04e27f30ed2446acdecade61c0a
CagyJiao <kaijie.jiao@instructure.com>
2021-09-19 11:57:27 +0200

    render App via json-server

--- part2/jsonserver/src/App.js
+++ part2/jsonserver/src/App.js
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useState } from 'react';
+import Note from './components/Note'
 
-function App() {
+const App = ( {notes} ) => {
+
+  const [noteArr, setNoteArr] = useState(notes);
+  const [newNote, setNewNote] = useState('a new note...');
+  const [showAll, setShowAll] = useState(true);
+
+  const addNote = (event) => {
+    // prevents the default action of submitting a form
+    event.preventDefault();
+    console.log('button clicked', event.target);  
+
+    const noteObject = {
+      content: newNote,
+      data: new Date().toISOString(),
+      import: Math.random() < 0.5,
+      id: notes.length + 1
+    };
+
+    setNoteArr(notes.concat(noteObject));
+    setNewNote('');
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event);
+    setNewNote(event.target.value);
+  }
+
+  const notesToShow = showAll 
+                        ? notes
+                        : notes.filter(note => note.important === true);
   return (
-    <div className="App">
-      
+    <div>
+      <h1>Notes</h1>
+
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'importand' : 'all'}
+        </button>
+      </div>
+
+      <ul>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      {/* <ul>
+        {notes.map((note, i) =>
+          <li key={i}>
+            {note.content}
+          </li>
+        )}
+      </ul> */}
+
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
--- part2/jsonserver/src/index.js
+++ part2/jsonserver/src/index.js
@@ -1,30 +1,34 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import axios from 'axios';
 
-const promise = axios.get('http://localhost:3001/notes');
-console.log(promise);
-// response value
-promise.then(response => {
-  console.log(response);
-})
-// data value
-promise.then(response => {
-  const notes = response.data;
-  console.log(notes);
-})
+// const promise = axios.get('http://localhost:3001/notes');
+// console.log(promise);
+// // response value
+// promise.then(response => {
+//   console.log(response);
+// })
+// // data value
+// promise.then(response => {
+//   const notes = response.data;
+//   console.log(notes);
+// })
+
 // chained format
 axios
   .get('http://localhost:3001/notes')
   .then(response => {
     const notes = response.data;
     console.log(notes);
+    
+    ReactDOM.render(
+      <App notes={notes}/>,
+    document.getElementById('root')
+    );
   })
 
 
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+
 
