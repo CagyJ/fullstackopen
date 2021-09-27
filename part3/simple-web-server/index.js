e5ad8c0cff89bfd78a169460a102f6b2a1c3b9b8
CagyJiao <kaijie.jiao@instructure.com>
2021-09-27 14:44:18 +0200

    express and fetching a resource

--- part3/simple-web-server/index.js
+++ part3/simple-web-server/index.js
@@ -1,11 +1,14 @@
 // console.log('hello world')
 
-const http = require('http')
+// const http = require('http')
+
+const express = require('express')
+const app = express()
 
 let notes = [
     {
       id: 1,
-      content: "HTML is easy",
+      content: "HTML is easy?",
       date: "2019-05-30T17:30:31.098Z",
       important: true
     },
@@ -23,12 +26,39 @@ let notes = [
     }
 ]
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' })
-    // response.end('Hello World')
-    response.end(JSON.stringify(notes))
+// const app = http.createServer((request, response) => {
+//     response.writeHead(200, { 'Content-Type': 'text/plain' })
+//     // response.end('Hello World')
+//     response.end(JSON.stringify(notes))
+// })
+
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+// automatically set the Content-Type be application/json
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+// use colon syntax to define parameters in express
+app.get('/api/notes/:id', (request, response) => {
+    // the para should be 'string'
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    
+    if (note) {
+        response.json(note)
+    } else {
+        // display 404 error
+        response.status(404).end()
+    }
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+// app.listen(PORT)
+// console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
