602cbdccd303681a22b3b82a86fa59ecfb47889f
CagyJiao <kaijie.jiao@instructure.com>
2021-09-30 11:06:02 +0200

    fix undefined issue via add the path of .env

--- part3/nodes-backend-mongodb/index.js
+++ part3/nodes-backend-mongodb/index.js
@@ -1,51 +1,20 @@
-require('dotenv').config
+require('dotenv').config({
+    path: `${__dirname}/mongodb.env`
+})
 
 const express = require('express')
 const app = express()
-
-const mongoose = require('mongoose')
+const Note = require('./models/note')
 
 app.use(express.json())
 
 const cors = require('cors')
 app.use(cors())
 
-
-const url = process.env.MONGODB_URI
-console.log('connecting to', url)
-
-mongoose.connect(url)
-.then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-
-const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean
-})
-
-// change toJSON method, delete the __v and __id, also set __id to id property
-noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
-
-const Note = mongoose.model('Node', noteSchema)
-
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
-// automatically set the Content-Type be application/json
 app.get('/api/notes', (request, response) => {
     Note.find({}).then(result => {
         console.log(result);
@@ -53,55 +22,55 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-// use colon syntax to define parameters in express
-app.get('/api/notes/:id', (request, response) => {
-    // the para should be 'string'
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
+// // use colon syntax to define parameters in express
+// app.get('/api/notes/:id', (request, response) => {
+//     // the para should be 'string'
+//     const id = Number(request.params.id)
+//     const note = notes.find(note => note.id === id)
     
-    if (note) {
-        response.json(note)
-    } else {
-        // display 404 error
-        response.status(404).end()
-    }
-})
+//     if (note) {
+//         response.json(note)
+//     } else {
+//         // display 404 error
+//         response.status(404).end()
+//     }
+// })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+// app.delete('/api/notes/:id', (request, response) => {
+//     const id = Number(request.params.id)
+//     notes = notes.filter(note => note.id !== id)
 
-    response.status(204).end()
-})
+//     response.status(204).end()
+// })
 
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
+// const generateId = () => {
+//     const maxId = notes.length > 0
+//         ? Math.max(...notes.map(n => n.id))
+//         : 0
+//     return maxId + 1
+// }
 
-app.post('/api/notes', (request, response) => {
+// app.post('/api/notes', (request, response) => {
 
-    const body = request.body
+//     const body = request.body
 
-    if (!body.content) {
-        return response.status(400).json( {
-            error: 'content missing'
-        })
-    }
+//     if (!body.content) {
+//         return response.status(400).json( {
+//             error: 'content missing'
+//         })
+//     }
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    }
+//     const note = {
+//         content: body.content,
+//         important: body.important || false,
+//         date: new Date(),
+//         id: generateId(),
+//     }
 
-    notes = notes.concat(note)
+//     notes = notes.concat(note)
 
-    response.json(note)
-})
+//     response.json(note)
+// })
 
 const PORT = process.env.PORT
 // app.listen(PORT)
