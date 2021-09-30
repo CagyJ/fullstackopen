2a64fddb49deceee9c0f020ff723bb37942112b6
CagyJiao <kaijie.jiao@instructure.com>
2021-09-30 12:00:17 +0200

    delete and put

--- part3/nodes-backend-mongodb/index.js
+++ part3/nodes-backend-mongodb/index.js
@@ -10,7 +10,6 @@ const cors = require('cors')
 app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
-app.use(requestLogger)
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -36,12 +35,13 @@ app.get('/api/notes/:id', (request, response, next) => {
         
 })
 
-// app.delete('/api/notes/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     notes = notes.filter(note => note.id !== id)
-
-//     response.status(204).end()
-// })
+app.delete('/api/notes/:id', (request, response) => {
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
@@ -62,6 +62,22 @@ app.post('/api/notes', (request, response) => {
     note.save().then(savedNote => {
         response.json(savedNote)
     })
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    // {new: true} means the findByIdAndUpdate method will return the new object, not the original one
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
